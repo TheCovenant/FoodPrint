5a28fb4739b12d62876b652c89cfb274c87cd6dd
TheCovenant <cfaluyi@gmail.com>
2019-03-16 16:46:22 -0400

    added searchResult activity

--- app/src/main/java/com/example/hackrpi/foodprint/searchResult.java
+++ app/src/main/java/com/example/hackrpi/foodprint/searchResult.java
@@ -2,15 +2,12 @@ package com.example.hackrpi.foodprint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class History extends AppCompatActivity {
+public class searchResult extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
+        setContentView(R.layout.activity_search_result);
     }
-
-    String hello = "supppp";
 }
